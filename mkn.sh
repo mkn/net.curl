bcfe841f5b46059309d78b5c97a0ae87a44dda60
dekken <philip.deegan@gmail.com>
2017-11-05 19:18:18 +0100

    inti

---
+++ mkn.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+[ -z "$MKN_MAKE_THREADS" ] && MKN_MAKE_THREADS="$(nproc --all)"
+
+CURL="curl-7.56.1"
+
+GZ_FILE="${CURL}.tar.gz"
+
+[ ! -f "$GZ_FILE" ] && wget https://curl.haxx.se/download/${GZ_FILE}
+
+[ ! -d "$CURL" ] && tar xf $GZ_FILE
+
+rm -rf build
+mkdir -p build
+
+cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=$CWD/inst -DCMAKE_BUILD_TYPE=Release ../$CURL
+
+make -j${MKN_MAKE_THREADS}
+make install
